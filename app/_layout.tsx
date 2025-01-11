168fdd971d146c65e2dc9aa701a44048a3bcd5a3
s-egge <102624422+s-egge@users.noreply.github.com>
2025-01-11 08:24:58 -0800

    Drizzle Integration (#19)
    * Database works - useLiveQuery doesn't
    
    * Fixed useLiveQuery
    
    * Cleaned up code, removed RefreshCalendar button
    
    - We no longer need to refresh the calendar manually since Drizzle's useLiveQuery will trigger an update when the data changes
    - Changed getDay query from a select to findFirst so that it doesn't return an array. We can change this back if we end up wanting to have multiple entries for a single day in the db

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,19 +4,30 @@ import * as SplashScreen from "expo-splash-screen"
 import { StatusBar } from "expo-status-bar"
 import { useEffect } from "react"
 import "react-native-reanimated"
-import { openDatabase } from "@/db/database"
 import {
   PaperProvider,
   MD3LightTheme as DefaultTheme,
   MD3DarkTheme as DarkTheme,
 } from "react-native-paper"
-import { useColorScheme } from "react-native"
+import { useColorScheme, View, Text } from "react-native"
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context"
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator"
+import migrations from "@/drizzle/migrations"
+import { SQLiteProvider } from "expo-sqlite"
+import { Suspense } from "react"
+import { ActivityIndicator } from "react-native"
+import { useDrizzleStudio } from "expo-drizzle-studio-plugin"
+import { getDatabase, getDrizzleDatabase } from "@/db/database"
 
+const DB_NAME = "testing.db"
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
+  const expoDb = getDatabase()
+  const db = getDrizzleDatabase()
+  useDrizzleStudio(expoDb)
+  const { success, error } = useMigrations(db, migrations)
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   })
@@ -24,29 +35,50 @@ export default function RootLayout() {
   const theme = useColorScheme() == "dark" ? DarkTheme : DefaultTheme
 
   useEffect(() => {
-    if (loaded) {
+    if (loaded && success) {
       SplashScreen.hideAsync()
     }
-    openDatabase()
-  }, [loaded])
+  }, [loaded, success])
 
   if (!loaded) {
     return null
   }
 
+  if (error) {
+    console.error(error)
+    return (
+      <PaperProvider theme={theme}>
+        <SafeAreaProvider>
+          <SafeAreaView
+            style={{ flex: 1, backgroundColor: theme.colors.background }}
+          >
+            <View>
+              <Text>Database migration error: {error.message}</Text>
+            </View>
+            <StatusBar style="auto" />
+          </SafeAreaView>
+        </SafeAreaProvider>
+      </PaperProvider>
+    )
+  }
+
   return (
-    <PaperProvider theme={theme}>
-      <SafeAreaProvider>
-        <SafeAreaView
-          style={{ flex: 1, backgroundColor: theme.colors.background }}
-        >
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen name="+not-found" />
-          </Stack>
-          <StatusBar style="auto" />
-        </SafeAreaView>
-      </SafeAreaProvider>
-    </PaperProvider>
+    <Suspense fallback={<ActivityIndicator size="large" />}>
+      <SQLiteProvider databaseName={DB_NAME} useSuspense>
+        <PaperProvider theme={theme}>
+          <SafeAreaProvider>
+            <SafeAreaView
+              style={{ flex: 1, backgroundColor: theme.colors.background }}
+            >
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+              </Stack>
+              <StatusBar style="auto" />
+            </SafeAreaView>
+          </SafeAreaProvider>
+        </PaperProvider>
+      </SQLiteProvider>
+    </Suspense>
   )
 }
---
+++ drizzle/0000_polite_genesis.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `days` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`date` text NOT NULL,
+	`flow_intensity` integer
+);
