bf08d9f8f988508591b871c60893b58401bd2233
Blake <39657294+Blake-Hua@users.noreply.github.com>
2025-01-14 16:03:44 -0800

    Privacy Screen component implemented

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,24 @@
-import { Text } from "react-native-paper"
+import { View } from "react-native"
+import { Text, List, Divider } from "react-native-paper"
 import { ThemedView } from "@/components/ThemedView"
+import { Link } from "expo-router"
 
 export default function Settings() {
   return (
     <ThemedView style={{ height: "100%", padding: 10 }}>
-      <Text variant="titleLarge" style={{ textAlign: "center" }}>
-        Settings will go in here
+      <Text variant="titleLarge" style={{ marginBottom: 20, textAlign: "center" }}>
+        Settings
       </Text>
+      
+      <List.Section>
+        <Link href="/routers/PrivacyPolicyRouter" asChild>
+          <List.Item
+            title="Privacy Policy"
+            right={props => <List.Icon {...props} icon="chevron-right" />}
+          />
+        </Link>
+        <Divider />
+      </List.Section>
     </ThemedView>
   )
 }
