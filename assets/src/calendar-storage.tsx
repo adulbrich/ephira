38642a05e05a899bf35cf9627a8986ea2ae60671
lacim <lacimonsrud@gmail.com>
2025-01-21 15:06:59 -0800

    added calendar-storage.tsx

---
+++ assets/src/calendar-storage.tsx
@@ -0,0 +1,62 @@
+import {create} from 'zustand';
+import {Accordion, DayData, DayDataStore, LoadData, MarkedDates, Mood, Symptoms, Values} from '@/constants/Interfaces'
+
+const initialDay : DayData = {
+  id: 0,
+  date: "",
+  flow_intensity: 0, 
+}
+
+//creates DayDataStore interface to store the Selected Day Information
+export const useSelectedDate = create <DayDataStore>((set) => ({
+    ...initialDay,
+    setData: (data : DayData) => { set(() => 
+        ({date : data.date, id: data.id, flow_intensity: data.flow_intensity, is_cycle_start: data.is_cycle_start, is_cycle_end: data.is_cycle_end, notes: data.notes}))
+    },
+    setDate: (date : string) => { set(() =>
+        ({date : date}))
+    },
+    setId: (num : number) => { set(() =>
+      ({id: num}))
+    },
+    setFlow: (flow: number) => { set(() =>
+    ({flow_intensity: flow}))
+    },
+    reset: () => set(initialDay)
+  }))
+
+export const useMarkedDates = create <MarkedDates>((set) => ({}))
+
+export const useData = create<LoadData>((set) => ({
+  data: [],
+  show: false,
+  setData: (data : DayData[]) => { set(() => 
+    ({data : data}))
+  },
+  setShow: (show : boolean) => { set(() => 
+    ({show : show}))
+  }
+}))
+
+const initialString = null
+
+
+export const useAccordion = create<Accordion>((set)=>({state: initialString,
+  setExpandedAccordion: (data: string | null) => set(() => ({state: data}))
+}
+))
+
+export const useMoods = create<Mood>((set)=>({selectedMoods: [],
+  setSelectedMoods: (values: string[]) => set(() => ({selectedMoods: values}))
+}
+))
+
+export const useSymptoms = create<Symptoms>((set)=>({selectedSymptoms: [],
+  setSelectedSymptoms: (values: string[]) => set(() => ({selectedSymptoms: values}))
+}
+))
+
+export const useValues = create<Values>((set)=>({selectedValues: [],
+  setSelectedValues: (values: string[]) => set(() => ({selectedValues: values}))
+}
+))
