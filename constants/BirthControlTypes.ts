6fb820ad5b43e6d018c79e540eeb363c93fc1c6c
arcanda3 <arcanda@oregonstate.edu>
2025-01-24 18:44:41 -0800

    Implemented birth control and medication tracking, still need to test and refactor

---
+++ constants/BirthControlTypes.ts
@@ -0,0 +1,8 @@
+export const birthControlOptions = [
+    { label: "IUD", value: "iud" },
+    { label: "Pill", value: "pill" },
+    { label: "Ring", value: "ring" },
+    { label: "Patch", value: "patch" },
+    { label: "Shot", value: "shot" },
+    { label: "Implant", value: "implant" },
+];
---
+++ constants/Medications.ts
@@ -0,0 +1,18 @@
+export const medicationOptions = [
+    { label: "Pain Killers", value: "pain_killers" },
+    { label: "Antibiotics", value: "antibiotics" },
+    { label: "Cold & Flu", value: "cold_and_flu" },
+    { label: "Cough & Congestion", value: "cough_congestion" },
+    { label: "Emergency Contraception", value: "emergency_contraception" },
+    { label: "Antidepressants", value: "antidepressants" },
+    { label: "Anti-inflammatory Drugs", value: "anti_inflammatory" },
+    { label: "Blood Pressure", value: "blood_pressure" },
+    { label: "Diabetes", value: "diabetes_medication" },
+    { label: "Mood Stabilizers", value: "mood_stabilizers" },
+    { label: "Cholesterol", value: "cholesterol_medication" },
+    { label: "Asthma Inhalers", value: "asthma_inhalers" },
+    { label: "Sleep Aids", value: "sleep_aids" },
+    { label: "Anti-Anxiety", value: "anti_anxiety" },
+    { label: "Allergy", value: "allergy" },
+    { label: "Other", value: "other" },
+];
