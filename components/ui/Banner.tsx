73715abb341074fedf666b826636d02df35fd3dc
Blake <39657294+Blake-Hua@users.noreply.github.com>
2025-01-28 14:47:20 -0800

    added banner with app name and logo

--- components/ui/Banner.tsx
+++ components/ui/Banner.tsx
@@ -1,32 +1,35 @@
-import { View } from "react-native"
-import { Text, useTheme } from "react-native-paper"
-import { Image } from "react-native"
+import { Platform, View } from "react-native";
+import { Text } from "react-native-paper";
+import { Image } from "react-native";
 
 export default function Banner() {
-  const theme = useTheme()
   return (
-    <View style={{ 
-      flexDirection: 'row', 
-      alignItems: 'center', 
-      padding: 10,
-    }}>
-      <Image 
-        source={require('@/assets/images/icon.png')} 
-        style={{ 
-          width: 40, 
-          height: 40, 
+    <View
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+        padding: 10,
+      }}
+    >
+      <Image
+        source={require("@/assets/images/capstone-app-logo-transparent.png")}
+        style={{
+          width: 40,
+          height: 40,
           marginRight: 10,
-          borderRadius: 8
-        }} 
+          borderRadius: 8,
+        }}
       />
-      <Text 
-        variant="headlineMedium" 
-        style={{ 
-          fontWeight: 'bold'
+      <Text
+        variant="headlineMedium"
+        style={{
+          fontWeight: "bold",
+          fontStyle: Platform.OS === "ios" ? "italic" : undefined, 
+          fontFamily: Platform.OS === "android" ? "Roboto-Italic" : undefined,
         }}
       >
-        Ephira
+        ephira
       </Text>
     </View>
-  )
-} 
+  );
+}
