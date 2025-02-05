6f0faf520772975c3597aa255ace9ca305f6cdef
arcanda3 <arcanda@oregonstate.edu>
2025-02-04 19:05:24 -0800

    updated and formatted privacy policy

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,16 +3,21 @@ import AuthenticationSettings from "@/components/AuthenticationSettings";
 import PrivacyPolicy from "@/components/ui/PrivacyPolicy";
 import DeleteData from "@/components/DeleteData";
 import { Text } from "react-native-paper";
+import { ScrollView, SafeAreaView } from "react-native";
 
 export default function Settings() {
   return (
     <ThemedView style={{ height: "100%", padding: 10 }}>
-      <Text variant="titleLarge" style={{ textAlign: "center" }}>
-        Settings
-      </Text>
-      <AuthenticationSettings />
-      <DeleteData />
-      <PrivacyPolicy />
+      <SafeAreaView style={{ flex: 1 }}>
+        <Text variant="titleLarge" style={{ textAlign: "center" }}>
+          Settings
+        </Text>
+        <ScrollView contentContainerStyle={{ paddingBottom: 60 }}>
+          <AuthenticationSettings />
+          <DeleteData />
+          <PrivacyPolicy />
+        </ScrollView>
+      </SafeAreaView>
     </ThemedView>
   );
 }
