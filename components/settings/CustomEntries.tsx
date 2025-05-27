b8523a11b0b5468cbf9afed0bd8b2587e2fa002b
Blake <39657294+Blake-Hua@users.noreply.github.com>
2025-05-27 12:53:28 -0700

    added commas for formatting issues

--- components/settings/CustomEntries.tsx
+++ components/settings/CustomEntries.tsx
@@ -228,14 +228,14 @@ function CustomEntriesModal({
     const fetchSymptoms = async () => {
       const symptoms = await getAllSymptoms();
       const customSymptoms = symptoms.filter(
-        (symptom) => !symptomOptions.includes(symptom.name)
+        (symptom) => !symptomOptions.includes(symptom.name),
       );
       setSymptoms(customSymptoms.map((symptom) => symptom.name));
     };
     const fetchMoods = async () => {
       const moods = await getAllMoods();
       const customMoods = moods.filter(
-        (mood) => !moodOptions.includes(mood.name)
+        (mood) => !moodOptions.includes(mood.name),
       );
       setMoods(customMoods.map((mood) => mood.name));
     };
@@ -245,13 +245,13 @@ function CustomEntriesModal({
         (medication) =>
           !medicationOptions.includes(medication.name) &&
           !birthControlOptions.includes(medication.name) &&
-          medication.type !== "birth control"
+          medication.type !== "birth control",
       );
       const customBirthControl = medications.filter(
         (medication) =>
           !medicationOptions.includes(medication.name) &&
           !birthControlOptions.includes(medication.name) &&
-          medication.type === "birth control"
+          medication.type === "birth control",
       );
       setMedications(customMedications.map((medication) => medication.name));
       setBirthControl(customBirthControl.map((bc) => bc.name));
@@ -276,7 +276,7 @@ function CustomEntriesModal({
       ...birthControlOptions,
     ];
     const squashedAllEntries = allEntries.map((entry) =>
-      entry.replace(/[_\s]/g, "").toLowerCase()
+      entry.replace(/[_\s]/g, "").toLowerCase(),
     );
     if (squashedAllEntries.includes(squashedEntryName)) {
       setSnackbarText(`${entryName} already exists.`);
@@ -327,12 +327,12 @@ function CustomEntriesModal({
     // check if entry is in calendar filters and remove if needed
     if (selectedFilters.includes(entryName)) {
       const updatedFilters = selectedFilters.filter(
-        (filter: string) => filter !== entryName
+        (filter: string) => filter !== entryName,
       );
       setSelectedFilters(updatedFilters);
       updateSetting(
         SettingsKeys.calendarFilters,
-        JSON.stringify(selectedFilters)
+        JSON.stringify(selectedFilters),
       );
     }
   };
