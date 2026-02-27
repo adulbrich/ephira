90fb5cd4389550c745ceb9c9fc35862ac43f0be8
Hau'olinalani O'Brien <ulapulelehua@gmail.com>
2026-02-27 00:00:58 -0800

    Fixing eslint and other small issues to pass all checks

--- components/settings/WalkthroughReplay.tsx
+++ components/settings/WalkthroughReplay.tsx
@@ -1,20 +1,19 @@
-import { List, Divider, Text, useTheme } from "react-native-paper";
+import { List } from "react-native-paper";
 import { ThemedView } from "@/components/ThemedView";
 import { router } from "expo-router";
 
 export default function WalkthroughReplay() {
-  const theme = useTheme();
-
   return (
     <ThemedView style={{ height: "100%" }}>
       <List.Section>
         <List.Accordion
-          title="View App Walkthrough"
-          titleStyle={{ fontSize: 30 }}
+          title="Replay Walkthrough"
+          titleStyle={{ fontSize: 20 }}
           expanded={false}
           onPress={() => router.push("/(onboarding)/tour" as any)}
-          children={null}
-        />
+        >
+          {/* empty on purpose */}
+        </List.Accordion>
       </List.Section>
     </ThemedView>
   );
